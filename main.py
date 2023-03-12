b1d9236c15e8c916bf765c43099a2c494006363d
CoKeFish <37970518+CoKeFish@users.noreply.github.com>
2023-03-11 19:43:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import json
 import serial
 import time
 
-hw_sensor = serial.Serial(port='COM7', baudrate=2000000, timeout=1, write_timeout=1)
+from io import open
+
+hw_sensor = serial.Serial(port='COM6', baudrate=9600, timeout=1, write_timeout=1)
 
 if __name__ == '__main__':
-    #while True:
-        raw_string_b = hw_sensor.readline()
-        print(raw_string_b)
-        raw_string_b = hw_sensor.readline()
-        print(raw_string_b)
+    # while True:
+    raw_string_b = hw_sensor.readline()
+    print(len(raw_string_b))
