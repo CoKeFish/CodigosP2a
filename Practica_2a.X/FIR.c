40c22a4258a31fbc176488a95e71ccd03126c9d3
CoKeFish <37970518+CoKeFish@users.noreply.github.com>
2023-03-29 11:17:56 -0500

    Modi?

--- Practica_2a.X/FIR.c
+++ Practica_2a.X/FIR.c
@@ -1,24 +1,31 @@
+//
+// Created by rrtc2 on 26/03/2023.
+//
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N_COEFFS 5  // número de coeficientes del filtro FIR
-#define N_BUFFER 8
-#define MASK_Bufer 7
+//#include "10hzorden7 8bL 10bIL.h"
+#include "15hzorden11 12bL 10bL.h"
+//#include "20hzorden17 14bL 10bL.h"
+//#include "25hzorden30 12bL 10bL.h"
 
-unsigned long FIR(int* coeffs, int input) 
-{
+//#define BL 5  // número de coeficientes del filtro FIR
+#define N_BUFFER 32
+#define MASK_Bufer (N_BUFFER - 1)
+
+unsigned long FIR(const int *coeffs, int input) {
     static int buffer[N_BUFFER] = {0}; // buffer de entrada
     static int index = 0; // índice del buffer
-    static int inpar = N_COEFFS & 1;
+    static int inpar = BL & 1;
     unsigned long output = 0; // valor de salida
 
     // agregar nuevo valor de entrada al buffer y actualizar el índice
     buffer[index] = input;
 
     // calcular la salida del filtro
-    for (int i = 0; i < N_COEFFS; i++) {
-        output += (unsigned long)coeffs[i] * (unsigned long)buffer[(index - i) & MASK_Bufer];
+    for (int i = 0; i < BL; i++) {
+        output += (unsigned long) coeffs[i] * (unsigned long) buffer[(index - i) & MASK_Bufer];
     }
 
     index = (index + 1) & MASK_Bufer;
@@ -27,28 +34,7 @@ unsigned long FIR(int* coeffs, int input)
 
 
 
-unsigned long FIR_p(int* coeffs, int input) 
-{
-
-    static int index = 0; // índice del buffer
-    
-// buffer de entrada// buffer de entrada
-int* buffer = (int*)calloc(N_BUFFER, sizeof(int));
-    unsigned long output = 0; // valor de salida
-
-    // agregar nuevo valor de entrada al buffer y actualizar el índice
-    *(buffer + index) = input;
-
-    // calcular la salida del filtro
-    for (int i = 0; i < N_COEFFS; i++) {
-        output += (unsigned long)*(coeffs + i) * ((unsigned long)*(buffer + ((index - i) & MASK_Bufer)));
-    }
-
-    index = (index + 1) & MASK_Bufer;
-    return output;
-}
-
-unsigned long FIR_pE(int *coeffs, int input) {
+unsigned long FIR_p(const uint32_t *coeffs, int input) {
     // buffer de entrada
     static int buffer[N_BUFFER] = {0};
 
@@ -56,15 +42,15 @@ unsigned long FIR_pE(int *coeffs, int input) {
     static int *endBuffer = buffer + MASK_Bufer;    //Puntero de la cola
 
     static int *head = buffer;    //Puntero iterante de la cabeza
-    static int *tail = buffer + ((-N_COEFFS) & MASK_Bufer) ;    //Puntero iterante de la cabeza
+    static int *tail = buffer + ((-BL) & MASK_Bufer) ;    //Puntero iterante de la cabeza
 
 
     static int *head_i = buffer;    //Puntero iterante de la cabeza
-    static int *tail_i = buffer + ((-N_COEFFS + 1) & MASK_Bufer);    //Puntero iterante de la cola
+    static int *tail_i = buffer + ((-BL + 1) & MASK_Bufer);    //Puntero iterante de la cola
 
 
     static int indexHead = 0; // índice del buffer de la cabeza
-    static int indexTail = ((-N_COEFFS + 1) & MASK_Bufer); // índice del buffer de la cabeza
+    static int indexTail = ((-BL + 1) & MASK_Bufer); // índice del buffer de la cabeza
 
     unsigned long output = 0; // valor de salida
 
@@ -73,7 +59,7 @@ unsigned long FIR_pE(int *coeffs, int input) {
     *(head) = input;
 
     // calcular la salida del filtro
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < BL/2; i++) {
 
         output += (unsigned long) *(coeffs++) * ((unsigned long) * (head_i) + (unsigned long) * (tail_i));
         if(head_i == startBuffer)
@@ -107,4 +93,10 @@ unsigned long FIR_pE(int *coeffs, int input) {
     tail_i = tail;
 
     return output;
+}
+
+
+uint16_t FilterFIR(int input)
+{
+    return FIR_p(B, input) >> 16;
 }
--- Practica_2a.X/IIR.c
+++ Practica_2a.X/IIR.c
@@ -1,1 +1,22 @@
 
+
+
+float IIR_p(int* coeffs, int input) 
+{
+    X_a[0] = (*Gaina) * input;
+    
+    w_a[0] = ((*Dena0) * X_a[0]) - ((*Dena1) * X_a[1]) - ((*Dena2) * X_a[2]);
+    Y_a = ((*Numa0) * W_a[0]) + ((*Numa1) * W_a[1]) + ((*Numa2) * W_a[2]);
+    W_a[2] = W_a[1];
+    W_a[1] = W_a[0];
+    
+    
+//    X_b[0] = (*Gainb) * Y_a;
+//    
+//    w_b[0] = ((*Denb0) * X_b[0]) - ((*Denb1) * X_b[1]) - ((*Denb2) * X_b[2]);
+//    Y_b = ((*Numb0) * W_b[0]) + ((*Numb1) * W_b[1]) + ((*Numb2) * W_b[2]);
+//    W_b[2] = W_b[1];
+//    W_b[1] = W_b[0];
+    
+    return 
+}
